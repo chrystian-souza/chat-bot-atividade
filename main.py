c6b3155f0a45c0fe6914018a73155bd28f6256f2
chrystian-souza <chrystian.souza.silveira@gmail.com>
2024-05-16 21:54:25 -0300

    foi modificado o código

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
-bot = ChatBot('Bot')
+# Criar a instância do chatbot
+chatbot = ChatBot('Canguru banguélo')
 
-trainer = ListTrainer(bot)
+# Treinamento do chatbot com o corpus de exemplo
+trainer = ChatterBotCorpusTrainer(chatbot)
 
-trainer.train([
-    'Oi b',
-    'Olá, como posso ajudar?',
-    'Estou procurando passagens aéreas.',
-    'Para qual destino e data você está procurando?',
-    'Preciso de uma passagem para Nova York no dia 20 de março.',
-    'Deixe-me verificar a disponibilidade... Sim, temos várias opções para Nova York em 20 de março. Você prefere alguma faixa de horário?',
-    'Prefiro voos pela manhã.',
-    'Temos um voo saindo às 08:00 e outro às 11:00. Qual você prefere?',
-    'Vou ficar com o das 08:00.',
-    'Perfeito! Posso reservar para você. Preciso apenas de alguns detalhes adicionais.',
-    'Ok, obrigado pela ajuda.',
-    'De nada! Estou aqui para ajudar. Tenha uma boa viagem!'
-            ])
+# Treinar o chatbot com o corpus em português
+trainer.train("chatterbot.corpus.portuguese")
 
+# Função principal para conversar com o bot
+def conversar():
+    print("Olá! Eu sou o ChatterBot. Digite algo para começar a conversar...")
+    while True:
+        try:
+            usuario = input("Você: ")
+            bot = chatbot.get_response(usuario)
+            print(f"Bot: {bot}")
 
-while True:
-    request = input('você :')
-    if request.lower() == 'ok':
-        print('Bot: tchau')
-        break
-    else:
-        response = bot.get_response(request)
-        print('Bot:', response)
+        except (KeyboardInterrupt, EOFError, SystemExit):
+            break
+
+if __name__ == "__main__":
+    conversar()
